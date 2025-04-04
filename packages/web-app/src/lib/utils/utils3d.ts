4e9cd6a29d7fb4035da82015b65e83c918abb72c
Josh Clarke <joshua@compusult.net>
2025-04-04 12:12:04 -0230

    Create body for Add To 3D requests

--- packages/web-app/src/lib/utils/utils3d.ts
+++ packages/web-app/src/lib/utils/utils3d.ts
@@ -1,17 +1,19 @@
 export const SUPPORTED_3D_TYPES = [
 	'WMS',
-	//'WMTS',
+	'WMTS',
 	'ESRI REST',
-	//'OGCMAP',
-	//'FEATURE',
-	//'COVERAGE',
-	//'CELESTIAL',
-	//'SENSORTHINGS',
-	//'GEOJSON',
-	//'KML',
-	//'3DTILES',
-	//'COG',
-	//'GPKG'
+	'OGCMAP',
+	'FEATURE',
+	'COVERAGE',
+	'CELESTIAL',
+	'SENSORTHINGS',
+	'GEOJSON',
+	'KML',
+	'3DTILES',
+	'COG',
+	//'GPKG',
+	//'SHAPEFILE'
+
 ];
 
 export function create3dRequestBody(properties: any, row: any): {} {
@@ -34,7 +36,19 @@ export function create3dRequestBody(properties: any, row: any): {} {
 				}
 			};
 		case 'WMTS':
-			return {};
+			return {
+				type: 'WMTS_SERVICE',
+				args: {
+					uid: properties.id,
+					credit: '',
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					},
+					capabilitiesUrl: row['url']
+				}
+			};
 		case 'ESRI REST':
 			return {
 				type: 'ARCGISWMS',
@@ -59,24 +73,176 @@ export function create3dRequestBody(properties: any, row: any): {} {
 				}
 			};
 		case 'OGCMAP':
-			return {};
+			return {
+				type: 'OGCMAP',
+				args: {
+					type: 'OgcMap',
+					uid: properties.id,
+					name: '',
+					show: true,
+					url: row['url'],
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					},
+					bounds: {
+						minX: properties.extent.west,
+						minY: properties.extent.south,
+						maxX: properties.extent.east,
+						maxY: properties.extent.north
+					},
+					description: '',
+					credit: ''
+				}
+			};
 		case 'FEATURE':
-			return {};
+			return {
+				type: 'FEATURE',
+				args: {
+					uid: properties.id,
+					url: row['url'],
+					title: '',
+					description: '',
+					wgs84BoundingBox: {
+						minX: properties.extent.west,
+						minY: properties.extent.south,
+						maxX: properties.extent.east,
+						maxY: properties.extent.north
+					},
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					}
+				}
+			};
 		case 'COVERAGE':
-			return {};
+			return {
+				type: 'COVERAGE',
+				args: {
+					uid: properties.id,
+					title: '',
+					url: row['url'],
+					description: '',
+					sourceLayerIndex: '',
+					id: '',
+					wgs84BoundingBox: {
+						minX: properties.extent.west,
+						minY: properties.extent.south,
+						maxX: properties.extent.east,
+						maxY: properties.extent.north
+					},
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					}
+				}
+			};
 		case 'CELESTIAL':
-			return {};
+			return {
+				"type": "CELESTIAL",
+				"args": {
+						"uid": properties.id,
+						"name": properties.title.en,
+						"show": true,
+						"type": "Celestial",
+						"url": row['url']
+				}
+			};
 		case 'SENSORTHINGS':
-			return {};
+			return {
+				type: 'SENSORTHINGS',
+				args: {
+					uid: properties.id,
+					url: row['url'],
+					title: properties.title.en,
+					description: properties.description.en,
+					wgs84BoundingBox: {
+						minX: properties.extent.west,
+						minY: properties.extent.south,
+						maxX: properties.extent.east,
+						maxY: properties.extent.north
+					},
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					}
+				}
+			};
 		case 'GEOJSON':
-			return {};
+			return {
+				type: 'GEOJSON',
+				args: {
+					uid: properties.id,
+					urlOrGeoJsonObject: row['url'],
+					title: properties.title.en,
+					description: properties.description.en,
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					}
+				}
+			};
 		case 'KML':
-			return {};
+			return {
+				type: 'KML',
+				args: {
+					uid: properties.id,
+					url: row['url'],
+					title: properties.title.en,
+					description: properties.description.en,
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					}
+				}
+			};
 		case '3DTILES':
-			return {};
+			return {
+				type: '3DTILES',
+				args: {
+					uid: properties.id,
+					url: row['url'],
+					title: properties.title.en,
+					description: properties.description.en,
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					},
+					show: true
+				}
+			};
 		case 'COG':
-			return {};
-		case 'GPKG':
-			return {};
+			return {
+				type: 'COG',
+				args: {
+					uid: properties.id,
+					url: row['url'],
+					name: properties.title.en,
+					description: properties.description.en,
+					projection: 'REPLACE_WITH_REAL_VALUE',
+					serviceInfo: {
+						serviceTitle: row['name'],
+						serviceId: properties.id,
+						serviceUrl: row['url']
+					}
+				}
+			};
+		case 'CATALOG':
+			return {
+				type: 'CATALOG',
+				args: {
+					uid: properties.id,
+					title: row['name'],
+					url: row['url'],
+					type: 'STAC'
+				}
+			};
 	}
 }
